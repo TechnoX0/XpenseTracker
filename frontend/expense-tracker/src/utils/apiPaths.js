fb0e0bd568880f40ff5a5e456e57761dbb7eeb0b
TechnoX0 <meroypaulvincent@gmail.com>
2025-06-15 02:46:48 +0800

    added url

--- frontend/expense-tracker/src/utils/apiPaths.js
+++ frontend/expense-tracker/src/utils/apiPaths.js
@@ -1,34 +1,35 @@
-export const BASE_URL = "http://localhost:8000";
+export const BASE_URL =
+    import.meta.env.VITE_BACKEND_URL || "http://localhost:8000";
 
 // utils/apiPaths.js
 export const API_PATHS = {
-  AUTH: {
-    LOGIN: "/api/v1/auth/login",
-    REGISTER: "/api/v1/auth/register",
-    GET_USER_INFO: "/api/v1/auth/getUser",
-  },
-  DASHBOARD: {
-    GET_DATA: "/api/v1/dashboard",
-  },
-  INCOME:{
-    ADD_INCOME: "/api/v1/income/add",
-    GET_ALL_INCOME: "/api/v1/income/get",
-    DELETE_INCOME: (incomeId) =>  `/api/v1/income/${incomeId}`,
-    DOWNLOAD_INCOME: `/api/v1/income/downloadexcel`,
-  },
-  EXPENSE:{
-    ADD_EXPENSE: "/api/v1/expense/add",
-    GET_ALL_EXPENSE: "/api/v1/expense/get",
-    DELETE_EXPENSE: (expenseId) =>  `/api/v1/expense/${expenseId}`,
-    DOWNLOAD_EXPENSE: `/api/v1/expense/downloadexcel`,
-  },
-  IMAGE: {
-    UPLOAD_IMAGE: "/api/v1/auth/upload-image",
-  },
-  CATEGORY: {
-    ADD: "/api/v1/category/add",
-    GET_ALL: "/api/v1/category/get",
-    UPDATE: (id) => `/api/v1/category/${id}`,
-    DELETE: (id) => `/api/v1/category/${id}`,
-  },
+    AUTH: {
+        LOGIN: "/api/v1/auth/login",
+        REGISTER: "/api/v1/auth/register",
+        GET_USER_INFO: "/api/v1/auth/getUser",
+    },
+    DASHBOARD: {
+        GET_DATA: "/api/v1/dashboard",
+    },
+    INCOME: {
+        ADD_INCOME: "/api/v1/income/add",
+        GET_ALL_INCOME: "/api/v1/income/get",
+        DELETE_INCOME: (incomeId) => `/api/v1/income/${incomeId}`,
+        DOWNLOAD_INCOME: `/api/v1/income/downloadexcel`,
+    },
+    EXPENSE: {
+        ADD_EXPENSE: "/api/v1/expense/add",
+        GET_ALL_EXPENSE: "/api/v1/expense/get",
+        DELETE_EXPENSE: (expenseId) => `/api/v1/expense/${expenseId}`,
+        DOWNLOAD_EXPENSE: `/api/v1/expense/downloadexcel`,
+    },
+    IMAGE: {
+        UPLOAD_IMAGE: "/api/v1/auth/upload-image",
+    },
+    CATEGORY: {
+        ADD: "/api/v1/category/add",
+        GET_ALL: "/api/v1/category/get",
+        UPDATE: (id) => `/api/v1/category/${id}`,
+        DELETE: (id) => `/api/v1/category/${id}`,
+    },
 };
